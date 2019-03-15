1596a59dd02497955cad238776f2f13567f70cd5
Gael Monachon <gmonacho@student.le-101.fr>
2019-03-15 21:26:43 +0100

    libft

---
+++ libft/includes/get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   get_next_line.h                                  .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: gmonacho <gmonacho@student.le-101.fr>      +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/10/15 12:50:32 by gmonacho     #+#   ##    ##    #+#       */
+/*   Updated: 2018/11/07 17:58:05 by gmonacho    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 100
+
+# include <stdlib.h>
+# include <unistd.h>
+# include "libft.h"
+# include <fcntl.h>
+
+#endif
---
+++ libft/includes/libft.h
@@ -0,0 +1,113 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   libft.h                                          .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: gmonacho <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/10/03 10:23:36 by gmonacho     #+#   ##    ##    #+#       */
+/*   Updated: 2019/02/07 17:11:04 by gmonacho    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <stdlib.h>
+# include <stdio.h>
+
+typedef struct		s_list
+{
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}					t_list;
+
+void				ft_bzero(void *s, size_t n);
+void				ft_striter(char *s, void (*f)(char *));
+void				ft_strdel(char **as);
+void				*ft_memccpy(void *dst, const void *src, int c, size_t n);
+void				*ft_memchr(const void *s, int c, size_t n);
+void				*ftmemcpy(void *dst, const void *src, size_t n);
+void				*ft_memset(void *b, int c, size_t len);
+void				*ft_memalloc(size_t size);
+void				*ft_memmove(void *dst, const void *src, size_t len);
+void				ft_putchar(char c);
+void				ft_putchar_fd(char c, int fd);
+void				ft_putendl(const char *s);
+void				ft_putendl_fd(const char *s, int fd);
+void				ft_putnbr(int n);
+void				ft_putnbr_fd(int n, int fd);
+void				ft_putstr(const char *s);
+void				ft_putstr_fd(const char *s, int fd);
+void				ft_strclr(char *s);
+void				ft_striteri(char *s, void (*f)(unsigned int, char*));
+void				ft_memdel(void **ap);
+void				*ft_memset(void *b, int c, size_t len);
+void				*ft_memcpy(void *dst, const void *src, size_t n);
+void				ft_lstadd(t_list **alst, t_list *new);
+void				ft_lstaddend(t_list **alst, t_list *new);
+void				ft_lstdel(t_list **alst, void (*del)(void*, size_t));
+void				ft_lstdelone(t_list **alst, void (*del)(void*, size_t));
+void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
+void				ft_lstrev(t_list **alst);
+void				ft_lstswap(t_list **lst1, t_list **lst2);
+void				ft_swap(int *elem1, int *elem2);
+void				ft_2dintdel(int ***atab, int len);
+void				ft_2dputint(int **tab, int ilen, int jlen);
+void				ft_tabputint(int *tab, int len);
+void				ft_addtofloat(float *nb, float add);
+void				ft_intdel(int **tab);
+void				ft_2dstrdel(char ***tab);
+
+char				*ft_strdup(const char *s1);
+char				*ft_strcpy(char *dst, const char *src);
+char				*ft_strncpy(char *dst, const char *src, size_t len);
+char				*ft_strcat(char *s1, const char *s2);
+char				*ft_strncat(char *s1, const char *s2, size_t n);
+char				*ft_strnew(size_t len);
+char				*ft_strchr(const char *s, int c);
+char				*ft_strrcher(const char *s, int c);
+char				*ft_strstr(const char *haystack, const char *needle);
+char				*ft_strnstr(const char *haystack,
+			const char *needle, size_t len);
+char				*ft_strtrim(const char *s);
+char				**ft_strsplit(const char *s, char c);
+char				*ft_strjoin(const char *s1, const char *s2);
+char				*ft_itoa(int n);
+char				*ft_strsub(char const *s, unsigned int start, size_t len);
+char				*ft_strmap(char const *s, char (*f)(char));
+char				*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char				*ft_strrchr(const char *s, int c);
+
+size_t				ft_strlen(const char *s);
+size_t				ft_strlcat(char *dst, const char *src, size_t size);
+size_t				ft_lstlen(t_list **alst);
+size_t				ft_tablen(void **tab);
+
+int					ft_memcmp(const void *s1, const void *s2, size_t n);
+int					ft_atoi(const char *str);
+double				ft_abs(double n);
+int					ft_isalnum(int c);
+int					ft_isalpha(int c);
+int					ft_isascii(int c);
+int					ft_isdigit(int c);
+int					ft_isprint(int c);
+int					ft_tolower(int c);
+int					ft_toupper(int c);
+int					ft_strcmp(const char *s1, const char *s2);
+int					ft_strncmp(const char *s1, const char *s2, size_t n);
+int					ft_strequ(char const *s1, char const *s2);
+int					ft_strnequ(char const *s1, char const *s2, size_t n);
+int					ft_count_char(char *str, int c);
+int					**ft_2dintpushback(int **tab, int size_i, int size_j, int *line);
+int					get_next_line(const int fd, char **line);
+int					ft_index(const char *str, int c);
+int					ft_countnumbers(char *str);
+int					ft_round(double nb);
+
+t_list				*ft_lstnew(void const *content, size_t content_size);
+t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
+t_list				*ft_lstsplit(const char *s1, char c);
+
+#endif
