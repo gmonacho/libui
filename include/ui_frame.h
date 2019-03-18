ef0f4ce6ea4f5d4ed977c03c0910a6c79f00dbba
Gael Monachon <gmonacho@student.le-101.fr>
2019-03-18 15:53:42 +0100

    consversion frame en tableau

--- include/ui_frame.h
+++ include/ui_frame.h
@@ -8,7 +8,7 @@
 /*
 ** Les frames sont des elements incontournables,
 ** la plupart des interfaces et fonction d'affichage en dependent
-** Il est conseille de stocker les frames dans des listes chainees
+** Il est conseille de stocker les frames dans un tableau de structure dans leur ordre d'affichage
 ** L'affichage se fera du premier au dernier
 ** r : zone permettant de delimiter l'affichage ect
 ** *name : nom du frame (id sous forme de chaine de caractere)
@@ -17,24 +17,19 @@
 typedef struct	s_frame
 {
 	t_rect			r;
-	char			*name;
 	SDL_Texture		*texture;
 	t_btn 			*btn;
-	int 			n_btn;
-	struct s_frame	*next;
+	unsigned int	n_btn;
 	/*
 	**  Et pleins d'autres selon le besoin,
 	**  mais reste coherent @!&*@$ de ta race !
 	*/
 }					t_frame;
 
-
-t_frame		*ui_new_frame(t_rect rect,
-						char *name,
-						SDL_Texture *texture);
-void		ui_add_frame_back(t_frame **frames, t_frame *new_frame);
-void		ui_add_frame_n(t_frame **frames, t_frame *new_frame, int n);
-t_frame		*ui_get_frame(t_frame **frames, char *name);
+t_frame		ui_new_frame(t_rect rect, char *name, SDL_Texture *texture);
+t_frame		*ui_add_frame(t_frame *frames, t_frame new_frame, unsigned int n_frame);
+//void		ui_add_frame_n(t_frame **frames, t_frame *new_frame, int n);
+//t_frame		ui_get_frame(t_frame **frames, char *name);
 int			ui_add_button_to_frame(t_frame *frame, t_btn btn);
 
 #endif
