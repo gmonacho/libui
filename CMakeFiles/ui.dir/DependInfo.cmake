15b307dab5d60d4fe555b5a30ceb35aa87fb06bf
Gael Monachon <gmonacho@z1r5p2.le-101.fr>
2020-01-22 14:17:49 +0100

    ajout de CMakeFiles

---
+++ CMakeFiles/ui.dir/DependInfo.cmake
@@ -0,0 +1,78 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/gmonacho/Documents/101/libui/src/button/ui_button.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/button/ui_button.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/button/ui_simple_button.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/button/ui_simple_button.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/button/ui_text_area.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/button/ui_text_area.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/button/ui_text_entry_button.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/button/ui_text_entry_button.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/draw/draw.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/draw/draw.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/draw/draw_shape.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/draw/draw_shape.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/event/ui_event.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/event/ui_event.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/frame/ui_frame.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/frame/ui_frame.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/frame/ui_simple_frame.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/frame/ui_simple_frame.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/load_ui/ui_check_line_name.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/load_ui/ui_check_line_name.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/load_ui/ui_general_get.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/load_ui/ui_general_get.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/load_ui/ui_get_b_type.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/load_ui/ui_get_b_type.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/load_ui/ui_get_flags.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/load_ui/ui_get_flags.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/load_ui/ui_get_type.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/load_ui/ui_get_type.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/load_ui/ui_load.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/load_ui/ui_load.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/load_ui/ui_parse_button.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/load_ui/ui_parse_button.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/load_ui/ui_parse_buttons.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/load_ui/ui_parse_buttons.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/load_ui/ui_parse_frame.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/load_ui/ui_parse_frame.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/load_ui/ui_parse_frame_data.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/load_ui/ui_parse_frame_data.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/load_ui/ui_parse_simple_button.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/load_ui/ui_parse_simple_button.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/load_ui/ui_parse_simple_frame.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/load_ui/ui_parse_simple_frame.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/load_ui/ui_parse_text_area.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/load_ui/ui_parse_text_area.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/load_ui/ui_parse_text_entry_button.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/load_ui/ui_parse_text_entry_button.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/load_ui/ui_parse_text_entry_cut.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/load_ui/ui_parse_text_entry_cut.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/load_ui/ui_parse_texture.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/load_ui/ui_parse_texture.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/load_ui/ui_parse_type.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/load_ui/ui_parse_type.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/main.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/main.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/shape/ui_rect.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/shape/ui_rect.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/texture/ui_draw_one_line_return.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/texture/ui_draw_one_line_return.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/texture/ui_draw_text_line.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/texture/ui_draw_text_line.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/texture/ui_draw_text_line_return.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/texture/ui_draw_text_line_return.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/texture/ui_draw_text_rect.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/texture/ui_draw_text_rect.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/texture/ui_free_texture_set.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/texture/ui_free_texture_set.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/texture/ui_renderer.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/texture/ui_renderer.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/texture/ui_text.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/texture/ui_text.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/texture/ui_texture.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/texture/ui_texture.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/texture/ui_texture_list.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/texture/ui_texture_list.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/ui_error.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/ui_error.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/ui_init.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/ui_init.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/win/ui.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/win/ui.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/win/ui_call_button_function.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/win/ui_call_button_function.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/win/ui_display_buttons.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/win/ui_display_buttons.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/win/ui_display_frames.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/win/ui_display_frames.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/win/ui_display_simple.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/win/ui_display_simple.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/win/ui_display_text_area.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/win/ui_display_text_area.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/win/ui_display_text_entry.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/win/ui_display_text_entry.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/win/ui_get_side_rect.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/win/ui_get_side_rect.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/win/ui_mouse.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/win/ui_mouse.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/win/ui_resolve_buttons_event.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/win/ui_resolve_buttons_event.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/win/ui_resolve_text_entry_button.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/win/ui_resolve_text_entry_button.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/win/ui_set_button_function.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/win/ui_set_button_function.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/win/ui_update_buttons.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/win/ui_update_buttons.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/win/ui_update_frames.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/win/ui_update_frames.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/win/ui_win.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/win/ui_win.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/win/ui_win_size.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/win/ui_win_size.c.o"
+  "/Users/gmonacho/Documents/101/libui/src/win/ui_win_update_ui.c" "/Users/gmonacho/Documents/101/libui/CMakeFiles/ui.dir/src/win/ui_win_update_ui.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "include"
+  "libft/includes"
+  "/Users/gmonacho/.brew/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
